36b3498052d4275d9af6a52c4ff5531afece1c94
Fred Saba <fred@fred-saba.com>
2019-11-01 10:01:40 -0700

    added more style and reverted changes to fix new rounds

--- nier-memory-game/src/App.js
+++ nier-memory-game/src/App.js
@@ -1,78 +1,33 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Wrapper from './components/GameWrapper'
 import Title from './components/Title'
 import Card from './components/Card'
 import CardsWrapper from './components/CardsWrapper'
 import cardsData from './cards.json'
 import './index.css'
+import {Animated} from "react-animated-css";
+
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            selectedCards: [],
-            cards: [],
-            score: 0,
-            highscore: 0,
-            message: ""
-        }
-
+constructor(props){
+    super(props)
+    this.state = {
+        message: "",
+        cards : [],
+        score : 0,
+        highscore : 0
     }
 
-    componentDidMount() {
-        const storage = JSON.parse(localStorage.getItem("clickyGame")) || { highscore: 0 }
+}
+
+    componentDidMount(){
+        const storage = JSON.parse(localStorage.getItem("memoryGame")) || { highscore: 0 }
         this.setState({ highscore: storage.highscore, cards: this.shuffleCards(cardsData) })
-    }
-
-
-    gameOver = () => {
-
-        if (this.state.score > this.state.highscore) {
-            const storeData = JSON.stringify({ highscore: this.state.score })
-            localStorage.setItem("clickyGame", storeData)
-            this.setState({ highscore: this.state.score })
-        }
-
-        //this.state.cards.forEach(card => card.count = 0)
-        this.setState({ score: 0 })
 
     }
 
-    victoryCheck = () => {
-        let remainingCards = this.state.cards.filter(card => card.count === 0)
-        if (remainingCards.length === 0) {
-            this.setState({ message: 'You have selected all cards! Keep going to increase your score', cards: this.shuffleCards(cardsData) })
-            // this.state.cards.forEach(card => card.count = 0)
-        }
-
-    }
-
-
-    clickCount = id => {
-
-
-
-        let isDuplicate = false
-        let newCards = this.state.cards.map(card => {
-            if (card.id === id) {
-                if (this.state.selectedCards.includes(id)) {
-                    isDuplicate = true;
-                } else {
-                    this.setState({ selectedCards: [...this.state.selectedCards, id]})
-                }
-            }
-            return card;
-        })
-        if (isDuplicate) {
-            this.setState({ message: "You have lost!", cards: this.shuffleCards(cardsData), selectedCards: [], score: 0 })
-        } else {
-            this.setState({ message: "", cards: this.shuffleCards(newCards), score: this.state.score + 1 })
-            this.victoryCheck()
-        }
-    }
-
-    shuffleCards = original => {
-        let arr = [...original];
+    shuffleCards = data => {
+        let arr = [...data];
         for (let i in arr) {
             let randIndex = Math.floor(Math.random() * arr.length);
 
@@ -84,57 +39,96 @@ class App extends Component {
         }
         return arr
     }
+    
+    gameOver = () => {
 
-    render() {
+        if(this.state.score>this.state.highscore){
+            const storeData = JSON.stringify({ highscore: this.state.score })
+            localStorage.setItem("memoryGame", storeData)
+            this.setState({ highscore: this.state.score })
+        }
 
-        const { cards, score, highscore, message } = this.state
-
-        return (<Wrapper>
-            <Title score={score} highscore={highscore} message={message} />
-
-            <CardsWrapper>
-                {cards.map(card => (
-                    <Card
-                        clickCount={this.clickCount}
-                        id={card.id}
-                        key={card.id}
-                        name={card.name}
-                        image={card.image}
-                    />
-                ))}
-            </CardsWrapper>
-        </Wrapper>
-        )
-
+        this.setState({message: 'Game Over!'})
+        this.state.cards.forEach(card => card.count = 0)
+        this.setState({score : 0})
 
     }
-}
+
+    victoryCheck = () => {
+        let remainingCards = this.state.cards.filter(card => card.count === 0)
+        if(remainingCards.length === 0){
+            this.setState({message: 'You Have Selected All The Cards! Keep Going To Increase Your Score'})
+            this.state.cards.forEach(card => card.count = 0)
+        }
+        
+    }
+
+
+    clickCount = id => {
+        
+
+     
+        this.state.cards.find(card=>{
+            
+            if(card.id === id){
+                
+                if(card.count===0){
+                    
+                    this.setState({message: ''})
+                    this.setState({score : this.state.score + 1})
+                    card.count = card.count + 1
+                    this.setState({ cards: this.shuffleCards(this.state.cards) })
+                    this.victoryCheck()
+                    return true;
+                }
+                
+                else {
+                    this.gameOver()
+                    this.setState({ cards: this.shuffleCards(this.state.cards) })
+                    return true;
+                }
+            }
+            return false;
+        })
+            
+          }
+        
+          render() {
+            
+            const { cards, score, highscore, message } = this.state
+    
+            return (<Wrapper>
+                <Animated animationIn="fadeInDown" animationOut="fadeOut" isVisible={true}>
+
+                <Title score={score} highscore={highscore} message={message} />
+                </Animated>
+    
+               
+                <CardsWrapper>
+                    {cards.map(card => (
+                         <Animated animationIn="fadeInRight" animationOut="fadeOut" isVisible={true}>
+                        <Card
+                            clickCount={this.clickCount}
+                            id={card.id}
+                            key={card.id}
+                            name={card.name}
+                            image={card.image}
+                        />
+                        </Animated>
+                    ))}
+                    
+                </CardsWrapper>
+                
+            </Wrapper>
+            )
+    
+        }
+    }
+
+
+
+
+
+
 
 export default App
-
-
-
-
-        // this.state.cards.find(card => {
-
-        //     if (card.id === id) {
-
-        //         if (card.count === 0) {
-
-        //             this.setState({ score: this.state.score + 1 })
-        //             card.count = card.count + 1
-        //             this.victoryCheck()
-        //             this.setState({ message: "", cards: this.shuffleCards(this.state.cards) })
-        //             return true;
-        //         }
-
-        //         else {
-        //             this.setState({ message: 'You have lost!', cards: this.shuffleCards(cardsData) }, ()=>console.log(this.state))
-        //             this.gameOver()
-        //             return true;
-        //         }
-        //     }
-        //     return false;
-        // })
-// }
-    
