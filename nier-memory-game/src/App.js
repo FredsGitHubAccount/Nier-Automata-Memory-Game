d8566316fc0e9de0c0c463225c64920186e9c851
Fred Saba <fred@fred-saba.com>
2019-11-01 08:33:42 -0700

    modified code so it won't mutate state directly

--- nier-memory-game/src/App.js
+++ nier-memory-game/src/App.js
@@ -1,88 +1,108 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Wrapper from './components/GameWrapper'
 import Title from './components/Title'
 import Card from './components/Card'
 import CardsWrapper from './components/CardsWrapper'
-import cards from './cards.json'
+import cardsData from './cards.json'
 import './index.css'
 
 class App extends Component {
-constructor(props){
-    super(props)
-    this.state = {
-        cards,
-        score : 0,
-        highscore : 0
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedCards: [],
+            cards: [],
+            score: 0,
+            highscore: 0,
+            message: ""
+        }
+
     }
 
-}
+    componentDidMount() {
+        const storage = JSON.parse(localStorage.getItem("clickyGame")) || { highscore: 0 }
+        this.setState({ highscore: storage.highscore, cards: this.shuffleCards(cardsData) })
+    }
 
 
     gameOver = () => {
 
-        if(this.state.score>this.state.highscore){
-            this.setState({highscore:this.setState.highscore = this.state.score})
+        if (this.state.score > this.state.highscore) {
+            const storeData = JSON.stringify({ highscore: this.state.score })
+            localStorage.setItem("clickyGame", storeData)
+            this.setState({ highscore: this.state.score })
         }
 
-        this.state.cards.forEach(card => card.count = 0)
-        this.setState({score : 0})
+        //this.state.cards.forEach(card => card.count = 0)
+        this.setState({ score: 0 })
 
     }
 
     victoryCheck = () => {
-        let remainingCards = this.state.cards.filter(card=> card.count === 0)
-        if(remainingCards.length === 0){
-            alert('You have selected all cards! Keep going to increase your score')
-            this.state.cards.forEach(card => card.count = 0)
+        let remainingCards = this.state.cards.filter(card => card.count === 0)
+        if (remainingCards.length === 0) {
+            this.setState({ message: 'You have selected all cards! Keep going to increase your score', cards: this.shuffleCards(cardsData) })
+            // this.state.cards.forEach(card => card.count = 0)
         }
-        
+
     }
 
 
     clickCount = id => {
 
-     
-        this.state.cards.find(card=>{
-            
-            if(card.id === id){
-                
-                if(card.count===0){
-                    
-                    this.setState({score : this.state.score + 1})
-                    card.count = card.count + 1
-                    this.state.cards.sort(()=>Math.random() -.5)
-                    this.victoryCheck()
-                    return true;
-                }
-                
-                else {
-                    alert('You have lost!')
-                    this.gameOver()
-                    return true;
+
+
+        let isDuplicate = false
+        let newCards = this.state.cards.map(card => {
+            if (card.id === id) {
+                if (this.state.selectedCards.includes(id)) {
+                    isDuplicate = true;
+                } else {
+                    this.setState({ selectedCards: [...this.state.selectedCards, id]})
                 }
             }
-            return false;
+            return card;
         })
-            
-          }
-        
-      
-     
-    render(){
-        return(<Wrapper>
-            <Title score={this.state.score} highscore={this.state.highscore}/>
-            
+        if (isDuplicate) {
+            this.setState({ message: "You have lost!", cards: this.shuffleCards(cardsData), selectedCards: [], score: 0 })
+        } else {
+            this.setState({ message: "", cards: this.shuffleCards(newCards), score: this.state.score + 1 })
+            this.victoryCheck()
+        }
+    }
+
+    shuffleCards = original => {
+        let arr = [...original];
+        for (let i in arr) {
+            let randIndex = Math.floor(Math.random() * arr.length);
+
+            let temp1 = arr[randIndex];
+            let temp2 = arr[i];
+
+            arr[randIndex] = temp2;
+            arr[i] = temp1
+        }
+        return arr
+    }
+
+    render() {
+
+        const { cards, score, highscore, message } = this.state
+
+        return (<Wrapper>
+            <Title score={score} highscore={highscore} message={message} />
+
             <CardsWrapper>
-            {this.state.cards.map(card => (
-                <Card
-                    clickCount={this.clickCount}
-                    id={card.id}
-                    key={card.id}
-                    name={card.name}
-                    image={card.image}
-                />
-            ))}
-        </CardsWrapper>
+                {cards.map(card => (
+                    <Card
+                        clickCount={this.clickCount}
+                        id={card.id}
+                        key={card.id}
+                        name={card.name}
+                        image={card.image}
+                    />
+                ))}
+            </CardsWrapper>
         </Wrapper>
         )
 
@@ -90,10 +110,31 @@ constructor(props){
     }
 }
 
-
-
-
-
-
-
 export default App
+
+
+
+
+        // this.state.cards.find(card => {
+
+        //     if (card.id === id) {
+
+        //         if (card.count === 0) {
+
+        //             this.setState({ score: this.state.score + 1 })
+        //             card.count = card.count + 1
+        //             this.victoryCheck()
+        //             this.setState({ message: "", cards: this.shuffleCards(this.state.cards) })
+        //             return true;
+        //         }
+
+        //         else {
+        //             this.setState({ message: 'You have lost!', cards: this.shuffleCards(cardsData) }, ()=>console.log(this.state))
+        //             this.gameOver()
+        //             return true;
+        //         }
+        //     }
+        //     return false;
+        // })
+// }
+    
--- nier-memory-game/src/components/Title/index.js
+++ nier-memory-game/src/components/Title/index.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import './style.css'
 
-let Title = props =>(
+let Title = ({highscore, score, message}) => (
     <div className="title-wrapper">
-            <div className="logo-score-wrapper">
+        <div className="logo-score-wrapper">
             <div className="logo-wrapper">
                 <img className="nier-logo" alt="img" src="https://i.dlpng.com/static/png/5577261-nier-automata-logo-png-abeoncliparts-cliparts-vectors-for-nier-automata-logo-transparent-2120_900_preview.png"></img>
             </div>
-        <div>
-        <h2>Current Score : {props.score}</h2>
-        <h2>High Score : {props.highscore}</h2>
-        </div> 
+            <div className="score-wrapper">
+                <h2>Current Score : {score}</h2>
+                <h2>High Score : {highscore}</h2>
+                <h2>{message}</h2>
+            </div>
         </div>
 
         <div className="directions-wrapper">
-        <h1>Memory Game</h1>
-        <h2>Clicking A Unique Card Grants A Point & Will Shuffle The Cards!  Be Careful, You Can't Select The Same Card Twice!</h2>
+            <h1>Memory Game</h1>
+            <h2>Clicking A Unique Card Grants A Point & Will Shuffle The Cards!  Be Careful, You Can't Select The Same Card Twice!</h2>
         </div>
-     
-        
-        </div>
+
+
+    </div>
 )
 
 
